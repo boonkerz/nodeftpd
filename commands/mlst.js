c77e39f1643269f703532983127d0de21a49e030
Thomas Peterson <info@thomas-peterson.de>
2014-09-21 15:31:03 +0200

    pam

--- commands/mlst.js
+++ commands/mlst.js
@@ -30,6 +30,7 @@ command.add('MLST', 'MLST [<sp> pathname]', function (pathname, commandChannel, 
       facts += ';type=file';
     }
     console.log(unix.getGroup({ gid: stat.gid }));
+      console.log({ gid: stat.gid });
     facts += ';unique=' + facter.unique(stat);
     facts += ';UNIX.group=' + unix.getGroup({ gid: stat.gid })[0].group;
     facts += ';UNIX.gid=' + stat.gid;
